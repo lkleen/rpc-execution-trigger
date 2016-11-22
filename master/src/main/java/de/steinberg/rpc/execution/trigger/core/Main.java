ba2e174c95d25c09748d8e6e5385a9ae2cfe7bed
lkleen <github@larsworks.org>
2016-11-22 17:24:06 +0100

    project stub

---
+++ master/src/main/java/de/steinberg/rpc/execution/trigger/core/Main.java
@@ -0,0 +1,13 @@
+package de.steinberg.rpc.execution.trigger.core;
+
+/**
+ * Created by lkleen on 22.11.2016.
+ */
+public class Main {
+
+    public static void main (String... args)
+    {
+
+    }
+
+}
