7c0ef44b79bcbc7a65a9d669570d005f3554355f
lkleen <github@larsworks.org>
2016-11-23 15:17:38 +0100

    added network information output

--- master/src/main/java/de/steinberg/rpc/execution/trigger/master/Main.java
+++ master/src/main/java/de/steinberg/rpc/execution/trigger/master/Main.java
@@ -2,6 +2,7 @@ package de.steinberg.rpc.execution.trigger.master;
 
 import de.steinberg.rpc.execution.trigger.core.protocol.Message;
 import de.steinberg.rpc.execution.trigger.core.protocol.impl.SocketStringReceiver;
+import de.steinberg.rpc.execution.trigger.master.network.NetowrkInterfacesInfo;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -12,17 +13,17 @@ public class Main {
 
     public static void main (String... args)
     {
+        log.info("NETWORK INTERFACES");
+        log.info(NetowrkInterfacesInfo.create ());
+
         SocketStringReceiver receiver = new SocketStringReceiver();
         log.info("waiting for request");
 
-        try {
-            while (true) {
-                Message<String> message = receiver.receive();
-                log.info("received: {}", message.getValue());
-            }
-        } finally {
-            receiver.close();
+        while (true) {
+            Message<String> message = receiver.receive();
+            log.info("received: {}", message.getValue());
         }
+        //receiver.close();
     }
 
 }
---
+++ master/src/main/java/de/steinberg/rpc/execution/trigger/master/network/NetowrkInterfacesInfo.java
@@ -0,0 +1,35 @@
+package de.steinberg.rpc.execution.trigger.master.network;
+
+import de.steinberg.rpc.execution.trigger.core.exception.RpcExecutionTriggerException;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.util.Collections;
+
+/**
+ * Created by lkleen on 23.11.2016.
+ */
+public class NetowrkInterfacesInfo {
+
+    public static String create() {
+        try {
+            String interfaces = "";
+            for (NetworkInterface networkInterface : Collections.list(NetworkInterface.getNetworkInterfaces())) {
+                interfaces += "----------------------------------------\n";
+                interfaces += createInterfaceString (networkInterface);
+            }
+            return interfaces;
+        } catch (Exception e) {
+            throw new RpcExecutionTriggerException(e);
+        }
+    }
+
+    private static String createInterfaceString(NetworkInterface networkInterface) {
+        String str = networkInterface.getDisplayName() + "\n";
+        for (InetAddress address : Collections.list(networkInterface.getInetAddresses())) {
+            str += address.getHostName() + ": " + address.getHostAddress() + "\n";
+        }
+        return str;
+    }
+
+}
