d87c1e572c19606987d3deee077ca2862dc9facc
Lars Kleen <l.kleen@steinberg.de>
2017-01-10 18:13:38 +0100

    collects correct data but circular buffer does not work ass expected

--- WPR/startNTKernelLogger.bat
+++ WPR/startNTKernelLogger.bat
@@ -1,1 +1,1 @@
-logman start "NT Kernel Logger" -p "Windows Kernel Trace" (profile,proccntr,dpc,isr,syscall,disk,process,thread,file,dispatcher,pf,hf,img,cswitch) -bs 1024 -nb 128 128 -mode Circular -o kernel.etl -ct cycle -max 1024 -ets -rt
+logman start "NT Kernel Logger" -p "Windows Kernel Trace" (profile,proccntr,dpc,isr,syscall,disk,process,thread,file,dispatcher,pf,hf,img,cswitch) -bs 1024 -nb 128 128 -mode Circular -o kernel.etl -ct perf -max 512 -ets -rt
