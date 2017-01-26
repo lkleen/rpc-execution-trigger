851f3a53a6a6cee1b2b48530e524fc8c424923a7
Lars Kleen <github@larsworks.org>
2017-01-26 17:06:42 +0100

    created glow button

--- ui/src/test/java/javafx/webkit/test/GlowLabel.java
+++ ui/src/test/java/javafx/webkit/test/GlowLabel.java
@@ -3,27 +3,25 @@ package javafx.webkit.test;
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
-import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.effect.Effect;
 import javafx.scene.effect.Glow;
-import javafx.scene.input.MouseEvent;
 import javafx.util.Duration;
 
 /**
  * Created by lars on 26.01.2017.
  */
-public class MoinController {
+public class GlowLabel extends Label {
 
-    @FXML
-    Label label;
+    enum Color {
+        RED,
+        GREEN,
+        YELLOW
+    }
 
-    @FXML
     public void initialize() {
-
         Glow glow = new Glow();
         glow.setLevel(0);
-        label.setEffect(glow);
+        setEffect(glow);
         final Timeline timeline = new Timeline();
         timeline.setCycleCount(Timeline.INDEFINITE);
         timeline.setAutoReverse(true);
@@ -31,15 +29,14 @@ public class MoinController {
         final KeyFrame kf = new KeyFrame(Duration.millis(900), kv);
         timeline.getKeyFrames().add(kf);
         timeline.play();
-
-        label.setId("red-glow");
     }
 
-    public void mouseEntered(MouseEvent mouseEvent) {
-        label.setId("green-glow");
+    public void setColor(Color color) {
+        switch (color) {
+            case YELLOW: setId("yellow-glow"); break;
+            case RED: setId("red-glow"); break;
+            case GREEN: setId("green-glow"); break;
+        }
     }
 
-    public void mouseExited(MouseEvent mouseEvent) {
-        label.setId("yellow-glow");
-    }
 }
